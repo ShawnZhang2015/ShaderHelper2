4d12f4f98cd2b1902d6dcc7ceb8e9e2a0070db7d
zxh <zxh1982@qq.com>
2019-09-11 22:33:13 +0800

    增加说明

--- assets/components/ShaderHelper.js
+++ assets/components/ShaderHelper.js
@@ -10,9 +10,9 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        effect: cc.EffectAsset,
-        speed: 0.01,
-        props: [ShaderProperty],
+        effect: cc.EffectAsset,   //effect资源
+        speed: 0.01,              //控制动态Shader的time参数
+        props: [ShaderProperty],  //shader参数
     },
 
     start () {
@@ -20,22 +20,36 @@ cc.Class({
             return;
         }
 
+        //获取精灵组件
         let sprite = this.node.getComponent(cc.Sprite);
         if (!sprite) {
             return;    
         }
 
+        //实例化一个材质对象
         let material = new cc.Material();
+        //在材质对象上开启USE_TEXTURE定义
         material.define('USE_TEXTURE', true); 
+        //为材质设置effect，也是就绑定Shader了
         material.effectAsset = this.effect;
         material.name = this.effect.name;
-        sprite.setMaterial(0, material);
-        this.material = sprite.getMaterial(0);
-        this.time = 0;
 
+        //将材质绑定到精灵组件上，精灵可以绑定多个材质
+        //这里我们替换0号默认材质
+        sprite.setMaterial(0, material);
+
+        //从精灵组件上获取材质，这步很重要，不然没效果
+        this.material = sprite.getMaterial(0);
+
+        //初始化参数
+        this.time = 0;
         this.props.forEach(item => this.material.setProperty(item.key, item.value));
     },
 
+    /**
+     * 在update事件中更新材质参数
+     * 不同的Shader这里可能需要重写
+     */
     update () {
        
         if (!this.material || !this.speed) {
---
+++ readme.md
@@ -0,0 +1,5 @@
+## ShaderHelper 2.0.x 请看这里
+https://github.com/ShawnZhang2015/ShaderHelper
+
+## 更多详情关注公众号
+![Creator星球游戏开发社区](https://github.com/ShawnZhang2015/ShaderHelper2/raw/master/gzh.png)
